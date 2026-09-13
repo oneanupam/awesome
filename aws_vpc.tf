e3ee3b7aa76ac6acaf5c49df275a8ad8bd301e31
anupam. <43881706+oneanupam@users.noreply.github.com>
2026-09-13 23:19:37 +0100

    chore: kzb-02 update gitignore (#7)

--- aws_vpc.tf
+++
@@ -1,6 +0,0 @@
-# Resource Block to deploy Virtual Network
-resource "aws_vpc" "dev_vpc" {
-  cidr_block       = var.vpc_cidr
-  instance_tenancy = "default"
-  tags             = var.resource_tags
-}
