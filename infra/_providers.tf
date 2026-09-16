57d32b910d0b2933ffb9e36d2ab29938cd90e426
Anupam <anupaminit@gmail.com>
2026-09-16 09:44:52 +0100

    feat: kzb-07 update tf code

--- infra/_providers.tf
+++ infra/_providers.tf
@@ -1,6 +1,5 @@
 # Provider block to configure Azure Provider
 provider "aws" {
-  region     = var.aws_region
-  access_key = var.aws_cliusr_access_key
-  secret_key = var.aws_cliusr_secret_key
+  region  = var.aws_region
+  profile = "aws-lab"
 }
--- infra/_versions.tf
+++ infra/_versions.tf
@@ -1,11 +1,11 @@
 # Terraform block to configure Terraform and Provider Version
 terraform {
-  required_version = "~> 1.0.0" # To allow upgrade of 0.14.x versions.
+  required_version = "~> 1.15.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.70.0"
+      version = "~> 6.64"
     }
   }
 }
