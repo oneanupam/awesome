cf16a837145fb6fa55b10636089ebd8b9f1b077d
anupam. <43881706+oneanupam@users.noreply.github.com>
2026-09-20 17:40:36 +0100

    feat: kzb-10 update backend to use state lock (#17)

--- infra/_backend.tf
+++ infra/_backend.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "tf-states-bkt-200926"
-    key    = "fdn/bld/terraform.tfstate"
-    region = "eu-west-2"
+    bucket       = "tf-states-bkt-200926"
+    key          = "fdn/bld/terraform.tfstate"
+    region       = "eu-west-2"
+    use_lockfile = true
     assume_role = {
       role_arn = "arn:aws:iam::051131628466:role/local-tf-role"
     }
